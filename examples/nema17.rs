e2382834e65da8d5891c8c7f928cc787465463d7
ktechhydle <kellerhydle@gmail.com>
2026-05-10 11:18:21 -0600

    Examples Updates
    -fix misnaming and old info

--- examples/nema17.rs
+++ examples/nema17.rs
@@ -1,7 +1,7 @@
 use steperb::StepperController;
 
 fn main() {
-    // 1.8° per step (200 steps per revolution), 90° max range, -90° min range, initialized at 15°
+    // 1.8° per step (200 steps per revolution)
     let mut controller = StepperController::new(200);
     controller.set_desired_angle(45.0);
 
